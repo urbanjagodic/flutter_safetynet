17d22882c141c081cd93c5b6f3d5e7253d0d7f23
urban.jagodic <urban.jagodic@rekono.si>
2021-01-23 16:55:58 +0100

    Initial commit

---
+++ example/android/app/src/main/kotlin/com/urbanj/flutter_safetynet_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.urbanj.flutter_safetynet_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Classes/FlutterSafetynetPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterSafetynetPlugin : NSObject<FlutterPlugin>
+@end
